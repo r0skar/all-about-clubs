97e4889f96fed04fa6eaf561b9e5284797cb76e8
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-29 17:52:19 +0100

    add base layout & global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 import React from 'react'
+import { Main } from './components/Main'
+import { Header } from './components/Header'
 
-export const App: React.FC = () => <div>App</div>
+export const App: React.FC = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
