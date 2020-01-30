cc6851a63c5076d853da3d601c9bf41958294799
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-30 15:12:41 +0100

    add dynamic app title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
+import { matchPath, useLocation, useHistory } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import SortIcon from '@material-ui/icons/SortSharp'
+import ArrowBackIcon from '@material-ui/icons/ArrowBackSharp'
+import { APP_TITLE, HOME_ROUTE, DETAILS_ROUTE } from '../constants'
 import { useStore, SortOrder } from '../store'
+import { RouteParams } from '../types'
 
 export const Header: React.FC = () => {
+  const { pathname } = useLocation()
   const { state, dispatch } = useStore()
+  const { push: navigateTo } = useHistory()
+  const match = matchPath<RouteParams>(pathname, { path: DETAILS_ROUTE })
+  const { clubIndex = 0 } = match?.params || {}
+  const club = useMemo(() => state.content[Number(clubIndex)], [state.content, clubIndex])
 
   const toggleSorting = () => {
     return state.sortOrder === SortOrder.NAME
@@ -20,13 +29,24 @@ export const Header: React.FC = () => {
     <AppBar position="fixed">
       <Toolbar>
         <Grid container alignItems="center">
-          <Grid container item xs={8}>
-            <Typography component="h1" variant="h6">
-              all about clubs
-            </Typography>
+          <Grid container item xs={8} alignItems="center">
+            {pathname === HOME_ROUTE ? (
+              <Typography component="h1" variant="h6">
+                {APP_TITLE}
+              </Typography>
+            ) : (
+              <Fragment>
+                <IconButton color="inherit" onClick={() => navigateTo('/')}>
+                  <ArrowBackIcon />
+                </IconButton>
+                <Typography component="h1" variant="h6" display="inline">
+                  {club.name}
+                </Typography>
+              </Fragment>
+            )}
           </Grid>
           <Grid container item xs={4} justify="flex-end">
-            <IconButton color="inherit" onClick={toggleSorting}>
+            <IconButton color="inherit" onClick={() => pathname === HOME_ROUTE && toggleSorting()}>
               <SortIcon />
             </IconButton>
           </Grid>
